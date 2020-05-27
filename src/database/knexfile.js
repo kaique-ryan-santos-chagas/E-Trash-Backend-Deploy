46dec6e1b680554e19179d3a86e37026327abda3
Kaique Ryan <kaique.github@gmail.com>
2020-05-27 11:34:10 -0300

    Backend ready

--- src/database/knexfile.js
+++ src/database/knexfile.js
@@ -31,7 +31,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    connection: 'process.env.DATABASE_URL',
     ssl: {
       rejectUnauthorized: false
     },
